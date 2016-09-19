2c36a5a73d1c9b63fa3395c5c9582dee6831f244
rdalvarez <david_de_odin@hotmail.com>
2016-09-19 19:22:03 -0300

    intentar subir un archivo

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div>
+	<form method="POST" action="nexo.php" enctype="multipart/form-data">
+		<input type="text" name="ingreso" placeholder="Hola">
+		<br>
+		<input type="file" name="archivo" accept="image/*">
+		<br>
+		<input type="submit" name="Subir" value="Subir">
+	</form>
+</div>
+</body>
+</html>
