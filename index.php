497c73a8a68eb9fafc59e0f5cc5c5a81f6436291
rdalvarez <david_de_odin@hotmail.com>
2016-09-19 19:30:42 -0300

    Para ver el archivo se usa $_FILE

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <html>
 <head>
-	<title></title>
+	<title>Clase 05</title>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
 </head>
 <body>
-<div>
+<div class="CajaEnunciado">
 	<form method="POST" action="nexo.php" enctype="multipart/form-data">
 		<input type="text" name="ingreso" placeholder="Hola">
 		<br>
 		<input type="file" name="archivo" accept="image/*">
 		<br>
-		<input type="submit" name="Subir" value="Subir">
+		<input type="submit" name="Subir" value="Subir" class="MiBotonUTNMenuInicio">
 	</form>
 </div>
 </body>
